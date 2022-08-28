ba3a3da735552d87ba38b3a138c214fbb47b72e5
Fonsii <lualfonso14200174@gmail.com>
2022-08-28 17:16:36 -0600

    Photo in post and ordering in index

--- blog/user_app/models.py
+++ blog/user_app/models.py
@@ -6,4 +6,5 @@ class Post(models.Model):
     text = models.CharField(max_length=300)
     author = models.CharField(max_length=20)
     created_at = models.DateField()
+    photo = models.ImageField(upload_to='post/covers', default='post/covers/default.png')
 
--- blog/user_app/views/main_screen.py
+++ blog/user_app/views/main_screen.py
@@ -2,8 +2,8 @@ from django.views.generic.list import ListView
 from user_app.models import Post
 
 class MainScreen(ListView):
-
     model = Post
     template_name = "user_app/index.html"
     context_object_name = "context"
-    paginate_by = 5  # if pagination is desired
+    paginate_by = 4 
+    ordering = ['-created_at']
