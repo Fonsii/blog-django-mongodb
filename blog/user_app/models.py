4699d906716dc20888ec2b5b4c6c4966a81504f6
Fonsii <lualfonso14200174@gmail.com>
2022-08-31 18:01:14 -0600

    Removing the entry in post admin date

--- blog/user_app/models.py
+++ blog/user_app/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     text = models.CharField(max_length=300)
     author = models.CharField(max_length=20)
-    created_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
     photo = models.ImageField(upload_to='post/covers', default='post/covers/default.png')
 
--- blog/user_app/urls.py
+++ blog/user_app/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-from django.views.generic.base import View
 from . import views
 
 urlpatterns = [
